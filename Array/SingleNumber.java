78e2a93574b6a87a4e64da3ae3de39515471e5fb
urvichhabra2005 <urvichhabra2005@gmail.com>
2026-03-13 20:51:49 +0530

    Update SingleNumber.java

--- Array/SingleNumber.java
+++ Array/SingleNumber.java
@@ -1,23 +1,12 @@
-// TC - O(N)
+// TC-O(N)
 class Solution {
-    public int findKthPositive(int[] arr, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int c=1;
-        for(int i =1;i<=arr[arr.length-1]+k;i++)
+    public int singleNumber(int[] nums) {
+        int count =0;
+        for(int num : nums)
         {
-            boolean found = false;
-            for(int j =0;j<arr.length;j++)
-            {
-                if(arr[j]==i)
-                {
-                    found = true;
-                    break;
-                }
-            }
-                if(!found)
-                    map.put(c++,i);
+            count^=num;
+
         }
-        return map.get(k);
-        
+        return count;
     }
 }
